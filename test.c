e59d32dee5e0c4bb75fb009b3d24464157ab2a53
Archibald Thirion <archibald@archibalds-mbp.home>
2023-09-24 09:15:49 +0200

    init repo

---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+
+	printf("#include <stdio.h>%c%cint main(void) {%c%c%cprintf(\"#include <stdio.h>%c%cint main(void) {%c%c%c\", 10, 10, 10, 10, 9)\;", 10, 10, 10, 10, 9);
+	return (0);
+}
