2e990a559d7925b421304df4db8c295613438220
Archibald THIRION <athirion@made-f0Dr11s4.clusters.42paris.fr>
2023-09-26 13:47:51 +0200

    Colleen is done

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -6,9 +6,10 @@ global main
 extern printf
 
 code:
-    code DB "bits 64%2$c%2$c; Comment out%2$c%2$cglobal main%2$cextern printf%2$c%2$ccode:%2$c    code DB %3$c%1$s%3$c, 0%2$c%2$csection .text%2$c    quine:%2$c        push rbx%2$c        mov rax, 0%2$c        mov rdi, code%2$c        mov rsi, code%2$c        mov rdx, 10%2$c        mov rcx, 34%2$c        call printf wrt ..plt%2$c        pop rsi%2$c        ret%2$c    main:%2$c        jmp quine%2$c        ret%2$c", 0
+    code DB "bits 64%2$c%2$c; Comment out%2$c%2$cglobal main%2$cextern printf%2$c%2$ccode:%2$c    code DB %3$c%1$s%3$c, 0%2$c%2$csection .text%2$c    ; Comment in%2$c    quine:%2$c        push rbx%2$c        mov rax, 0%2$c        mov rdi, code%2$c        mov rsi, code%2$c        mov rdx, 10%2$c        mov rcx, 34%2$c        call printf wrt ..plt%2$c        pop rsi%2$c        ret%2$c    main:%2$c        jmp quine%2$c        ret%2$c", 0
 
 section .text
+    ; Comment in
     quine:
         push rbx
         mov rax, 0
