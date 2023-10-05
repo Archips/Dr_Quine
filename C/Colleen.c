1bac3fab269fa55086a37b3fa9a1e928dfb2e3d0
Archibald THIRION <athirion@paul-f4Br7s4.clusters.42paris.fr>
2023-10-05 11:26:46 +0200

    delete nl .c

--- C/Colleen.c
+++ C/Colleen.c
@@ -6,8 +6,8 @@
 
 void quine() {
 
-	char *code = "#include <stdio.h>%c%c/*%c  Comment out%c*/%c%cvoid quine() {%c%c	char *code = %c%s%c;%c	printf(code, 10, 10, 10, 10, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10,10,10,10,10,10, 10, 10);%c}%c%cint main(void) {%c%c	/*%c	  Comment in%c	*/%c%c	quine();%c%c	return (0);%c}";
-	printf(code, 10, 10, 10, 10, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10,10,10,10,10,10, 10, 10);
+	char *code = "#include <stdio.h>%c%c/*%c  Comment out%c*/%c%cvoid quine() {%c%c	char *code = %c%s%c;%c	printf(code, 10, 10, 10, 10, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10,10,10,10,10,10, 10, 10, 10);%c}%c%cint main(void) {%c%c	/*%c	  Comment in%c	*/%c%c	quine();%c%c	return (0);%c}";
+	printf(code, 10, 10, 10, 10, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10,10,10,10,10,10, 10, 10, 10);
 }
 
 int main(void) {
--- C/Grace.c
+++ C/Grace.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 #define GRACE "Grace_kid.c"
-#define CODE "#include <stdio.h>%c%c#define GRACE %cGrace_kid.c%c%c#define CODE %c%s%c%c#define QUINE() int main(void) {FILE *file = fopen(GRACE, %cw+%c); if (!file) return (0); fprintf(file,CODE,10,10,34,34,10,34,CODE,34,10,34,34,10,10,10,10,10,10,10); fclose(file); return (0);}%c%cQUINE()%c%c/*%c Grace is done !%c*/%c"
-#define QUINE() int main(void) {FILE *file = fopen(GRACE, "w+"); if (!file) return (0); fprintf(file,CODE,10,10,34,34,10,34,CODE,34,10,34,34,10,10,10,10,10,10,10); fclose(file); return (0);}
+#define CODE "#include <stdio.h>%c%c#define GRACE %cGrace_kid.c%c%c#define CODE %c%s%c%c#define QUINE() int main(void) {FILE *file = fopen(GRACE, %cw+%c); if (!file) return (0); fprintf(file,CODE,10,10,34,34,10,34,CODE,34,10,34,34,10,10,10,10,10,10); fclose(file); return (0);}%c%cQUINE()%c%c/*%c Grace is done !%c*/"
+#define QUINE() int main(void) {FILE *file = fopen(GRACE, "w+"); if (!file) return (0); fprintf(file,CODE,10,10,34,34,10,34,CODE,34,10,34,34,10,10,10,10,10,10); fclose(file); return (0);}
 
 QUINE()
 
