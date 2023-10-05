b5e9963afa74ab5fdeb0de6ab36226f0b90b241d
Archibald THIRION <athirion@paul-f4Br7s4.clusters.42paris.fr>
2023-10-05 11:57:09 +0200

    clean repo

--- test/grace/c/grace.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#define GRACE "Grace_kid.c"
-#define CODE "#include <stdio.h>%c%c#define GRACE %cGrace_kid.c%c%c#define CODE %c%s%c%c#define QUINE() int main(void) {FILE *file = fopen(GRACE, %cw+%c); if (!file) return (0); fprintf(file,CODE,10,10,34,34,10,34,CODE,34,10,34,34,10,10,10,10); fclose(file); return (0);}%c%cQUINE()%c%c// Grace is done !"
-#define QUINE() int main(void) {FILE *file = fopen(GRACE, "w+"); if (!file) return (0); fprintf(file,CODE,10,10,34,34,10,34,CODE,34,10,34,34,10,10,10,10); fclose(file); return (0);}
-
-QUINE()
-
-// Grace is done !
